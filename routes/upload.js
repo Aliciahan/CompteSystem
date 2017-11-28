c303cbe4cc8a160df91e75f2bfad81c09461e287
Alicia HAN <alicia.han007@gmail.com>
2017-11-28 20:45:26 +0100

    regular save

--- routes/upload.js
+++ routes/upload.js
@@ -6,6 +6,23 @@ var uuid = require('uuid');
 var Checks = require('../modules/checks');
 var Utils = require('../modules/utils');
 
-
+// Models data
 var PendingUpload = require('../models/pending_upload');
+var Piao = require('../models/piao');
+var Picture = require('../models/picture');
+
+
+// Function modules
 var FileSaver = require('../modules/filesaver');
+
+
+// Personal configurations
+var config = require('../config.json');
+
+
+
+
+
+
+
+
