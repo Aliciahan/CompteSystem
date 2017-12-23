b2b59d66c6b3c1b9ef18c4a4b126494ff619c003
zhifu <zhengzhifu0505@gmail.com>
2017-12-23 13:24:27 +0100

    kucun

--- public/javascripts/ViewCurrent.js
+++ public/javascripts/ViewCurrent.js
@@ -1,57 +1,59 @@
-app.controller('ViewCurrent', ['$scope','$http', 'Piao', '$uibModal', function($scope, $http, Piao, $uibModal){
+app.controller('ViewCurrent', ['$scope', '$http', 'Piao', '$uibModal', function($scope, $http, Piao, $uibModal) {
+    var vm = $scope;
 
-  $scope.focusingPiao={
-    "idNum": "请填写票号",
-    "bank": "ICBC",
-    "type": "dianpiao",
-    "amount": 8888,
-    "endDate": "1970-01-18T11:55:31.965Z",
-    "addDate": "1970-01-18T11:55:15.668Z",
-    "isSold": false,
-    "soldDate": "1970-01-18T11:55:15.668Z",
-    "headerPhoto": ""
-  };
+    vm.focusingPiao = {
+        "idNum": "请填写票号",
+        "bank": "ICBC",
+        "type": "dianpiao",
+        "amount": 8888,
+        "endDate": "1970-01-18T11:55:31.965Z",
+        "addDate": "1970-01-18T11:55:15.668Z",
+        "isSold": false,
+        "soldDate": "1970-01-18T11:55:15.668Z",
+        "headerPhoto": ""
+    };
 
-  $scope.piaos= Piao.piaos;
+    vm.piaos = Piao.piaos;
+    vm.countTotal;
+    vm.amountTotal;
 
-  $scope.getCurrentPiaos = function(){
-    Piao.getAllCurrent();
-    console.log($scope.piaos);
-  };
+    vm.getCurrentPiaos = function() {
+        Piao.getAllCurrent();
+        console.log(vm.piaos);
+    };
 
-  $scope.getFocus = function(piaoId){
-    $scope.focusingPiao = Piao.getById(piaoId);
-  };
+    vm.getFocus = function(piaoId) {
+        vm.focusingPiao = Piao.getById(piaoId);
+    };
 
 
-  $scope.popupDetail = function (piaoId) {
-    var parentElem = undefined;
-    var modalInst = $uibModal.open({
-      animation: true,
-      ariaLabelledBy: 'modal-title',
-      ariaDescribedBy: 'modal-body',
-      templateUrl: 'viewSinglePiao.html',
-      controller: 'ViewCurrentCtrl',
-      size: 'lg',
-      scope: $scope,
-      resolve: {
-        item: function(){
-          return piaoId;
+    vm.popupDetail = function(piaoId) {
+        var parentElem = undefined;
+        var modalInst = $uibModal.open({
+            animation: true,
+            ariaLabelledBy: 'modal-title',
+            ariaDescribedBy: 'modal-body',
+            templateUrl: 'viewSinglePiao.html',
+            controller: 'ViewCurrentCtrl',
+            size: 'lg',
+            scope: vm,
+            resolve: {
+                item: function() {
+                    return piaoId;
+                }
+            }
+        });
+
+    };
+
+    vm.delPiao = function(piaoId) {
+        if (confirm('你确定一定以及肯定么?')) {
+            Piao.delOneById(piaoId);
+        } else {
+
         }
-      }
-    });
-
-  };
-
-  $scope.delPiao = function(piaoId){
-    if(confirm('你确定一定以及肯定么?')){
-      Piao.delOneById(piaoId);
-    } else {
-
     }
-  }
 
-
-
+    vm.getCurrentPiaos();
 
 }]);
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -5,20 +5,20 @@
 var app = angular.module('frontapp', ['ui.router', 'ngAnimate', 'ngSanitize', 'base64', 'ui.bootstrap']);
 
 
-app.directive('fileModel', ['$parse', function ($parse) {
-  return {
-    restrict: 'A',
-    link: function(scope, element, attrs) {
-      var model = $parse(attrs.fileModel);
-      var modelSetter = model.assign;
+app.directive('fileModel', ['$parse', function($parse) {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            var model = $parse(attrs.fileModel);
+            var modelSetter = model.assign;
 
-      element.bind('change', function(){
-        scope.$apply(function(){
-          modelSetter(scope, element[0].files[0]);
-        });
-      });
-    }
-  };
+            element.bind('change', function() {
+                scope.$apply(function() {
+                    modelSetter(scope, element[0].files[0]);
+                });
+            });
+        }
+    };
 }]);
 
 
@@ -26,229 +26,229 @@ app.directive('fileModel', ['$parse', function ($parse) {
 
 
 app.factory('fileReader', ["$q", "$log", function($q, $log) {
-  var onLoad = function(reader, deferred, scope) {
-    return function() {
-      scope.$apply(function() {
-        deferred.resolve(reader.result);
-      });
-    }
-  }
-
-  var onError = function(reader, deferred, scope) {
-    return function() {
-      scope.$apply(function() {
-        deferred.reject(reader.result);
-      });
-    };
-  };
-
-  var getReader = function(deferred, scope) {
-    var reader = new FileReader();
-    reader.onload = onLoad(reader, deferred, scope);
-    reader.onerror = onError(reader, deferred, scope);
-    return reader;
-  };
-
-  var readAsDataURL = function(file, scope) {
-    var deferred = $q.defer();
-    var reader = getReader(deferred, scope);
-    reader.readAsDataURL(file);
-    return deferred.promise;
-  };
-
-  return {
-    readAsDataUrl: readAsDataURL
-  };
-}
-]);
-
-app.factory('auth', ['$http', '$window', function ($http, $window){
-
-  var auth = {};
-
-
-  auth.saveToken = function(token){
-    $window.localStorage['inscription-token'] = token;
-  };
-
-
-  auth.getToken = function() {
-    return $window.localStorage['inscription-token'];
-  };
-
-  auth.isAdmin = function () {
-    var  token = auth.getToken();
-    if(token){
-      var payload = JSON.parse($window.atob(token.split('.')[1]));
-      if (payload) {
-        return payload.role === "admin";
-      }else{
-        return false;
-      }
-    }else{
-      return false
-    }
-  };
-  auth.isLoggedin = function() {
-    var  token = auth.getToken();
-    if(token){
-      var payload = JSON.parse($window.atob(token.split('.')[1]));
-      if (payload) {
-
-        return payload.exp> Date.now()/1000;
-      }else{
-        return false;
-      }
-    }else{
-      return false
+    var onLoad = function(reader, deferred, scope) {
+        return function() {
+            scope.$apply(function() {
+                deferred.resolve(reader.result);
+            });
+        }
     }
 
-//        return false;
-//    }else {
-  //      return true;
-      //var payload = JSON.parse($window.atob(token.split('.')[1]));
-      //return payload.exp > Date.now()/1000;
-   // }
-  };
-
-  auth.current = function(){
-    if(auth.isLoggedin()){
-      var token = auth.getToken();
-      var payload = JSON.parse($window.atob(token.split('.')[1]));
-      return payload.usr;
-    }
-  };
-
-  auth.register = function (user){
-    return $http.post('http://localhost:3000/users/register', user).then(function(res){
-      auth.saveToken(res.data.token);
-    }, function(res){
-      console.log("err: "+res.data)
-    });
-  };
-
-  auth.adminreg = function (user){
-      return $http.post('http://localhost:3000/users/admin-reg', user).then(function(res){
-        auth.saveToken(res.data.token);
-      }, function(res){
-        console.log("err: "+res.data)
-      });
+    var onError = function(reader, deferred, scope) {
+        return function() {
+            scope.$apply(function() {
+                deferred.reject(reader.result);
+            });
+        };
     };
 
-  auth.logIn = function(user){
-    return $http.post('http://localhost:3000/users/login',user).then(function(res){
-      auth.saveToken(res.data.token);
-    });
-  };
+    var getReader = function(deferred, scope) {
+        var reader = new FileReader();
+        reader.onload = onLoad(reader, deferred, scope);
+        reader.onerror = onError(reader, deferred, scope);
+        return reader;
+    };
+
+    var readAsDataURL = function(file, scope) {
+        var deferred = $q.defer();
+        var reader = getReader(deferred, scope);
+        reader.readAsDataURL(file);
+        return deferred.promise;
+    };
+
+    return {
+        readAsDataUrl: readAsDataURL
+    };
+}]);
+
+app.factory('auth', ['$http', '$window', function($http, $window) {
+
+    var auth = {};
 
 
-  auth.logOut = function(){
-    console.log("we are here");
-    return $window.localStorage.removeItem("inscription-token");
-  };
+    auth.saveToken = function(token) {
+        $window.localStorage['inscription-token'] = token;
+    };
 
-  return auth;
+
+    auth.getToken = function() {
+        return $window.localStorage['inscription-token'];
+    };
+
+    auth.isAdmin = function() {
+        var token = auth.getToken();
+        if (token) {
+            var payload = JSON.parse($window.atob(token.split('.')[1]));
+            if (payload) {
+                return payload.role === "admin";
+            } else {
+                return false;
+            }
+        } else {
+            return false
+        }
+    };
+    auth.isLoggedin = function() {
+        var token = auth.getToken();
+        if (token) {
+            var payload = JSON.parse($window.atob(token.split('.')[1]));
+            if (payload) {
+
+                return payload.exp > Date.now() / 1000;
+            } else {
+                return false;
+            }
+        } else {
+            return false
+        }
+
+        //        return false;
+        //    }else {
+        //      return true;
+        //var payload = JSON.parse($window.atob(token.split('.')[1]));
+        //return payload.exp > Date.now()/1000;
+        // }
+    };
+
+    auth.current = function() {
+        if (auth.isLoggedin()) {
+            var token = auth.getToken();
+            var payload = JSON.parse($window.atob(token.split('.')[1]));
+            return payload.usr;
+        }
+    };
+
+    auth.register = function(user) {
+        return $http.post('http://localhost:3000/users/register', user).then(function(res) {
+            auth.saveToken(res.data.token);
+        }, function(res) {
+            console.log("err: " + res.data)
+        });
+    };
+
+    auth.adminreg = function(user) {
+        return $http.post('http://localhost:3000/users/admin-reg', user).then(function(res) {
+            auth.saveToken(res.data.token);
+        }, function(res) {
+            console.log("err: " + res.data)
+        });
+    };
+
+    auth.logIn = function(user) {
+        return $http.post('http://localhost:3000/users/login', user).then(function(res) {
+            auth.saveToken(res.data.token);
+        });
+    };
+
+
+    auth.logOut = function() {
+        console.log("we are here");
+        return $window.localStorage.removeItem("inscription-token");
+    };
+
+    return auth;
 }]);
 
 
-app.factory('Piao', ['$http','$base64',function($http, $base64){
-  var piaoObject = {
-    piaos : [],
-    one: {}
-  };
+app.factory('Piao', ['$http', '$base64', function($http, $base64) {
+    var piaoObject = {
+        piaos: [],
+        one: {}
+    };
 
-  piaoObject.delOneById = function(piaoId){
-    $http({
-      method: "DELETE",
-      url: '../piao/'+piaoId.toString()
-    }).then(function(res){
-      alert("Deleted!");
-      for(var i = (piaoObject.piaos.length -1); i>=0; i--){
-        if(piaoObject.piaos[i]._id === res.data._id){
-          console.log('index of piao :'+i.toString());
-          piaoObject.piaos.splice(i,1);
-        }
-      }
-      return res.data;
-    }, function(res){
-      alert("Error Delete Please make a phone call to Zhifu for solution~~");
-      return res.data;
-    })
-  };
+    piaoObject.delOneById = function(piaoId) {
+        $http({
+            method: "DELETE",
+            url: '../piao/' + piaoId.toString()
+        }).then(function(res) {
+            alert("Deleted!");
+            for (var i = (piaoObject.piaos.length - 1); i >= 0; i--) {
+                if (piaoObject.piaos[i]._id === res.data._id) {
+                    console.log('index of piao :' + i.toString());
+                    piaoObject.piaos.splice(i, 1);
+                }
+            }
+            return res.data;
+        }, function(res) {
+            alert("Error Delete Please make a phone call to Zhifu for solution~~");
+            return res.data;
+        })
+    };
 
-  piaoObject.getAll = function(){
-    return $http.get('../piao').then(function(res){
-      angular.copy(res.data, piaoObject.piaos)
-    }, function(res){
-      console.log(res.data.toString());
-    });
-  };
-
-  piaoObject.getAllCurrent = function(){
-    return $http.get('../piao/currentpiaos').then(function(res){
-      angular.copy(res.data, piaoObject.piaos)
-    }, function(res){
-      console.log(res.data.toString());
-    });
-  };
-
-  piaoObject.getById = function(piaoid){
-    $http.get('../piao/'+piaoid.toString()).then(function(res){
-      angular.copy(res.data, piaoObject.one);
-    }, function(res){
-      console.log("error:"+res.data);
-    })
-  };
-
-  piaoObject.create = function(piao){
-    return $http({
-      method: "POST",
-      url: "../piao",
-      data: piao
-    }).then(function(res){
-      piaoObject.piaos.push(res.data);
-      alert("上传成功, 你可以点击右下方按钮查看");
-    }, function(res){
-      alert("上传失败" + res.data.toString());
-    });
-  };
-
-  piaoObject.createPhoto = function(piao, picBin){
-    return $http({
-      method: "POST",
-      url: "../piao",
-      data: piao
-    })
-      .then(function(res){
-      var sendUrl = '../upload/'+res.headers("Location").toString().split('/').pop();
-      var fd = new FormData();
-      fd.append('file', picBin );
-      var ct = "image/";
-      if ( picBin.name.split('.').pop()==='jpeg'|| picBin.name.split('.').pop()==='jpg' ){
-        ct += 'jpeg'
-      } else {
-        ct+='png'
-      }
-      picBin.name = '@'+picBin.name;
-      console.log("sendUrl :"+ sendUrl);
-      $http({
-        method: "PUT",
-        url: sendUrl,
-        data: picBin,
-        headers: {
-          "Content-Type": ct
-        }
-      })
-        .then(function(res){
-          piaoObject.piaos.push(res.data);
-          console.log("upload success");
-          alert("上传成功, 你可以点击右下方按钮查看");
-        },
-        function myError(res) {
-          console.log("error");
-          alert("上传失败"+res.data.toString());
+    piaoObject.getAll = function() {
+        return $http.get('../piao').then(function(res) {
+            angular.copy(res.data, piaoObject.piaos)
+        }, function(res) {
+            console.log(res.data.toString());
         });
-    });
-  };
-  return piaoObject;
+    };
+
+    piaoObject.getAllCurrent = function() {
+        return $http.get('../piao/currentpiaos').then(function(res) {
+            angular.copy(res.data, piaoObject.piaos)
+            return res.data;
+        }, function(res) {
+            console.log(res.data.toString());
+        });
+    };
+
+    piaoObject.getById = function(piaoid) {
+        $http.get('../piao/' + piaoid.toString()).then(function(res) {
+            angular.copy(res.data, piaoObject.one);
+        }, function(res) {
+            console.log("error:" + res.data);
+        })
+    };
+
+    piaoObject.create = function(piao) {
+        return $http({
+            method: "POST",
+            url: "../piao",
+            data: piao
+        }).then(function(res) {
+            piaoObject.piaos.push(res.data);
+            alert("上传成功, 你可以点击右下方按钮查看");
+        }, function(res) {
+            alert("上传失败" + res.data.toString());
+        });
+    };
+
+    piaoObject.createPhoto = function(piao, picBin) {
+        return $http({
+                method: "POST",
+                url: "../piao",
+                data: piao
+            })
+            .then(function(res) {
+                var sendUrl = '../upload/' + res.headers("Location").toString().split('/').pop();
+                var fd = new FormData();
+                fd.append('file', picBin);
+                var ct = "image/";
+                if (picBin.name.split('.').pop() === 'jpeg' || picBin.name.split('.').pop() === 'jpg') {
+                    ct += 'jpeg'
+                } else {
+                    ct += 'png'
+                }
+                picBin.name = '@' + picBin.name;
+                console.log("sendUrl :" + sendUrl);
+                $http({
+                        method: "PUT",
+                        url: sendUrl,
+                        data: picBin,
+                        headers: {
+                            "Content-Type": ct
+                        }
+                    })
+                    .then(function(res) {
+                            piaoObject.piaos.push(res.data);
+                            console.log("upload success");
+                            alert("上传成功, 你可以点击右下方按钮查看");
+                        },
+                        function myError(res) {
+                            console.log("error");
+                            alert("上传失败" + res.data.toString());
+                        });
+            });
+    };
+    return piaoObject;
 }]);
